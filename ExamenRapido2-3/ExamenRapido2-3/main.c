d88d652a06583b4fa01083afe8a85d414b3d8ef3
Josue Garcia <avtelyes@gmail.com>
2015-10-15 12:46:43 -0500

    Examen rapido 2-3

---
+++ ExamenRapido2-3/ExamenRapido2-3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ExamenRapido2-3
+//
+//  Created by Josué Carlos García Puig on 15/10/15.
+//  Copyright © 2015 Avtelyes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
