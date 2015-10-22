6c9c8436f6678d699afd8d29ea02777826c70150
Josue Garcia <avtelyes@gmail.com>
2015-10-22 14:13:09 -0500

    Examen Rapido 2-4

---
+++ ExamenRapido2-4/ExamenRapido2-4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ExamenRapido2-4
+//
+//  Created by Josué Carlos García Puig on 22/10/15.
+//  Copyright © 2015 Josue Garcia. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
