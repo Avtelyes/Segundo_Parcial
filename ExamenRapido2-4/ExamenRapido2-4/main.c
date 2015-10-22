6351fb18cd20a5b7ada91e72070becc8551c87c2
Josue Garcia <avtelyes@gmail.com>
2015-10-22 15:27:29 -0500

    Examen Rapido 2-4

--- ExamenRapido2-4/ExamenRapido2-4/main.c
+++ ExamenRapido2-4/ExamenRapido2-4/main.c
@@ -25,7 +25,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t m_tabaco = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t m_papel = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t m_cerillo = PTHREAD_MUTEX_INITIALIZER;
-
+pthread_mutex_t m_ingredientes = PTHREAD_MUTEX_INITIALIZER;
 
 
 typedef struct{
@@ -73,49 +73,59 @@ void tomar_ingrediente(int i, Materiales * mat)
 {
     if(mat->cerillo==0)
     {
+        pthread_mutex_lock(&m_ingredientes);
         if(ingredientes[0] == 2 || ingredientes[1] == 2 )
         {
             pthread_mutex_trylock(&m_cerillo);
             mat->cerillo = 1;
             pthread_mutex_unlock(&m_cerillo);
         }
+        pthread_mutex_unlock(&m_ingredientes);
         
     }
     else if (mat->tabaco==0)
     {
+        pthread_mutex_lock(&m_ingredientes);
         if(ingredientes[0] == 0 || ingredientes[1] == 0 )
         {
             pthread_mutex_trylock(&m_tabaco);
             mat->tabaco = 1;
             pthread_mutex_unlock(&m_tabaco);
         }
+        pthread_mutex_unlock(&m_ingredientes);
         
     }
     else if (mat->papel==0)
     {
+        pthread_mutex_lock(&m_ingredientes);
         if(ingredientes[0] == 1 || ingredientes[1] == 1 )
         {
             pthread_mutex_trylock(&m_papel);
             mat->papel = 1;
             pthread_mutex_unlock(&m_papel);
         }
+        pthread_mutex_unlock(&m_ingredientes);
     }
     else
     {
+        pthread_mutex_lock(&m_ingredientes);
         fumar(i);
         mat->cerillo=0;
         mat->papel=0;
         mat->tabaco=0;
+        pthread_mutex_unlock(&m_ingredientes);
     }
 }
 
 void deja_ingredientes()
 {
+    pthread_mutex_lock(&m_ingredientes);
     int num = rand() % 3;
     int num2 = rand() % 3;
     
     ingredientes[0] = num;
     ingredientes[1] = num2;
+    pthread_mutex_unlock(&m_ingredientes);
 }
 
 void * fumador(void *arg){
@@ -125,11 +135,12 @@ void * fumador(void *arg){
     material->cerillo = 0;
     material->papel = 0;
     material->tabaco = 0;
+    int salida = 0;
     while (1){
         deja_ingredientes();
         tomar_ingrediente(i,material);
     }
-    //pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 
